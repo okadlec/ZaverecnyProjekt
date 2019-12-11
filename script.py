0d6d22f8f59f5c9c0428d3197f57fbe55621d512
shrek is love shrek is life <ondra.kadlec@email.cz>
2019-12-11 15:39:05 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,18 +1,34 @@
 import paho.mqtt.client as mqtt #import the client1
 import time
 import json
+import pymysql
 from datetime import datetime
-############
+
+x=0
+############                              
 def on_message(client, userdata, message):
     print("message received " ,str(message.payload.decode("utf-8")))
     print("message topic=",message.topic)
     presentime = datetime.now()
     data = {"topic": message.topic,"message": str(message.payload.decode("utf-8")),"time":presentime.strftime('%Y-%m-%d-%H-%M-%S')}
+    zprava = str(message.payload.decode("utf-8"))
+    cas = presentime.strftime('%Y-%m-%d-%H-%M-%S')
     json_file = open('personal.json', 'a+')
     json_file.write(str(data)+ '\n')
     print(data)
     json_file.close()
-
+    global x
+    connection = pymysql.connect(host='localhost',
+                      user='root',
+                      password='',
+                      db='fingerprint')
+    cursor = connection.cursor()
+    query = "INSERT INTO data(id,name,date) VALUES(" + "0"+",'"+zprava+"','" + cas+"');"
+    print(x)
+    print(query)
+    cursor.execute(query)
+    connection.commit()
+    x+=1
 ########################################
 broker_address="broker.mqtt-dashboard.com"
 #broker_address="iot.eclipse.org"
